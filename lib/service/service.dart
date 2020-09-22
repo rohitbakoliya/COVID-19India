ac0aea67bf9646c643631e404a4036974da38471
rohitbakoliya <bakoliyarohit00@gmail.com>
2020-09-22 15:26:03 +0530

    internet connection check with splash screen

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -5,8 +5,12 @@ import 'package:http/http.dart';
 
 class DataService{ 
   Future<Map> getData() async{
-    Response response = await get("https://api.covid19india.org/data.json");
-    final Map data =jsonDecode(response.body); 
-    return data;
+    try{
+      Response response = await get("https://api.covid19india.org/data.json");
+      final Map data =jsonDecode(response.body); 
+      return data;
+    }catch(e){
+      throw "Error found in service";
+    }
   }
 }
